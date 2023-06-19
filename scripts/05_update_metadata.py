854c04722217d9348d08a5afd4c6b629ccf2cd9f
Daniel Borek <daniel.borek@ugent.be>
2023-06-20 01:14:52 +0200

    chore: adding quarto R notebooks

--- scripts/05_update_metadata.py
+++ scripts/05_update_metadata.py
@@ -34,6 +34,18 @@ asop_keys = (
 # subjectID S T P ROI value
 # Where S is pre post sound, T is sham or real, P is pre post tDCS.
 
+columns_order = ['subjectID', 'S', 'T', 'P', 'ROI', 'regions']
+
+def standarize_roi_names(df: pd.DataFrame) -> pd.DataFrame:
+    return (
+    df
+    .pipe(lambda df: df.assign(region= df['ROI'].apply(lambda x: x.rsplit(' ', 1)[0]),
+                               hemi=df['ROI'].apply(lambda x: x.rsplit(' ', 1)[1])))
+    .replace({'hemi': {"L" :'l', "R" :'r'}})
+    .assign(label=lambda x: x['hemi'].str.cat(x['region'], sep='h_'))
+    .drop([ "region", "hemi"], axis=1)
+     )
+
 df_assr_standarized  = (
     pd.read_csv(f"{specparam_folder}/specparam_ASSR.csv")
     .replace({'iter_number': {1 :'pre', 3 :'pre', 2 :'post',4:'post'}})
@@ -42,12 +54,12 @@ df_assr_standarized  = (
     .merge(assr_keys, on='subject_cond')
     .assign(subjectID=lambda x: x['sub'].str.extract('(\d+)').astype(int))
     .drop([ "Unnamed: 0", "sub", "session", "subject_cond"], axis=1)
-    .pipe(lambda df: df.loc[:, ['subjectID','S', 'T', 'P', "ROI", 'regions', *df.columns.difference(['subjectID', 'S', 'T', 'P', 'ROI', 'regions'])]])
+    .pipe(lambda df: df.loc[:, [*columns_order, *df.columns.difference(columns_order)]])
     .sort_values(by=['subjectID', 'ROI'], ascending=[True, False])
+    .pipe(standarize_roi_names)
 )
 df_assr_standarized.to_csv(f"{specparam_folder}/specparam_ASSR_standarized.csv", index=False)
 
-# %%
 df_aspo_standarized= (
     pd.read_csv(f"{specparam_folder}/specparam_ASPO.csv")
     .replace({'iter_number': {1 :'pre', 3 :'pre', 2 :'post',4:'post'}})
@@ -57,7 +69,10 @@ df_aspo_standarized= (
     .merge(asop_keys, on=['subject', 'session'], how='left')
     .rename(columns={"iter_number": "P",  "labels": "ROI", "type": "T"})
     .drop([ "Unnamed: 0", "session", "subject"], axis=1)
-    .pipe(lambda df: df.loc[:, ['subjectID','S', 'T', 'P', "ROI", 'regions', *df.columns.difference(['subjectID', 'S', 'T', 'P', 'ROI', 'regions'])]])
+    .pipe(lambda df: df.loc[:, [*columns_order, *df.columns.difference(columns_order)]])
     .sort_values(by=['subjectID', 'ROI'], ascending=[True, False])
+    .pipe(standarize_roi_names)
+
 )
 df_aspo_standarized.to_csv(f"{specparam_folder}/specparam_ASPO_standarized.csv", index=False)
+# %%
